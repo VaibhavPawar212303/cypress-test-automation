89e59418847e85ecb258b6d151ddd22070fe3b5e
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-04-18 17:31:53 +0530

    added allure report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "cypress";
+import allureWriter from '@shelex/cypress-allure-plugin/writer';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       //setup the config for test as env 
       const version = config.env.version || "test";
       config.env = require(`./config/${version}.env.json`);
       config.baseUrl = config.env.baseUrl;
       return config;
+    },
+    reporter: 'cypress-allure-plugin',
+    reporterOptions: {
+      outputDir: 'allure-results',
+      clean: true,
     },
     viewportWidth: 1536,
     viewportHeight: 960,
