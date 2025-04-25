f3d47c426ed46ba1fa44041a4b13d79b536647a8
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-04-25 14:42:31 +0530

    added the fix for env setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 //@ts-ignore
 import { allureCypress } from "allure-cypress/reporter";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
@@ -8,22 +11,26 @@ export default defineConfig({
       allureCypress(on, config, {
         resultsDir: "allure-results",
       });
-       // accessing the environment variables 
-       config.env.REGION = process.env.REGION
-       config.env.ACCESS_KEY_ID = process.env.ACCESS_KEY_ID
-       config.env.SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY
 
-      // setup the config for test as env 
       const version = config.env.version || "test";
-      config.env = require(`./config/${version}.env.json`);
-      config.baseUrl = config.env.baseUrl;
+      const fileEnv = require(`./config/${version}.env.json`);
+
+      config.env = {
+        ...config.env,
+        ...fileEnv,
+        REGION: process.env.REGION,
+        ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+        SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+      };
+
+      if (config.env.baseUrl) {
+        config.baseUrl = config.env.baseUrl;
+      }
+
       return config;
     },
   },
   viewportWidth: 1536,
   viewportHeight: 960,
-  video:true
+  video: true,
 });
-
-
-
